f22f11bfd96dbbed11fd303bf6ade3a2c7462d23
Cole DeVries <colemandevries@gmail.com>
2022-10-10 14:59:15 -0400

    updated variable descriptions

--- variables.tf
+++ variables.tf
@@ -7,11 +7,11 @@ variable "app" {
 variable "env" {
   type        = string
   description = "The target environment for the stack - could be a tier or account level reference"
-  validation {
+    sensitive = false
+validation {
     condition     = contains(["dev", "qa", "stage", "prod", "demo", "perf", "nonprod", "prod"], var.env)
     error_message = "Must be one of the following: dev, qa, stage, prod, demo, perf,nonprod, prod"
   }
-  sensitive = false
 }
 
 variable "program" {
@@ -22,44 +22,60 @@ variable "program" {
 
 variable "glue_version" {
   type = string
+  description = "The version of glue to use, for example '1.0'"
+  sensitive = false
 }
 
 variable "max_retries" {
   type    = number
+  description = "The maximum number of times to retry this job if it fails"
   default = 0
+  sensitive = false
 }
 
 variable "worker_type" {
   type = string
+  description = "value"
+  sensitive = false
 }
 
 variable "number_of_workers" {
   type = number
+  description = "value"
+  sensitive = false
 }
 
 variable "timeout" {
   type    = number
   default = 10
+  sensitive = false
 }
 
 variable "execution_class" {
   type    = string
+  description = "The standard execution class is ideal for time-sensitive workloads that require fast job startup and dedicated resources. Valid value: FLEX, STANDARD"
   default = "FLEX"
+  sensitive = false
 }
 
 variable "description" {
   type = string
+  description = "Description of the job"
+  sensitive = false
 }
 
 variable "role_arn" {
   type = string
+  sensitive = false
 }
 
 variable "script_location" {
   type = string
+  sensitive = false
 }
 
 variable "default_arguments" {
   type    = map(string)
   default = null
+  sensitive = false
 }
